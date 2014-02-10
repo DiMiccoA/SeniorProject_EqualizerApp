fe0c335510ed6efeb40ae79702f4916d10fb6275
patrickoliver <oliverpa@onid.oregonstate.edu>
2014-02-10 11:40:59 -0800

    [WIP] Added basic Equalizer initialization
    Work In Progress (WIP) - Nothing actually works, but it's nice to look
    at. Hopefully I'll be able to get something to run soon.

--- Eclipse/src/com/fourears/equalizerapp/MainActivity.java
+++ Eclipse/src/com/fourears/equalizerapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.fourears.equalizerapp;
 
+import android.media.audiofx.Equalizer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	public SeekBar sb1,sb2,sb3,sb4,sb5;
+	private Equalizer equaliz;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setupEqualizerAndUI();
 	}
 
 	@Override
@@ -18,5 +25,19 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void setupEqualizerAndUI(){
+		equaliz = new Equalizer(0,0);
+		equaliz.setEnabled(true);
+		
+		TextView eqTextView = new TextView(this);
+        eqTextView.setText("Equalizer:");
+
+        short bands = equaliz.getNumberOfBands();
+        
+        
+        
+	}
+
 
 }
