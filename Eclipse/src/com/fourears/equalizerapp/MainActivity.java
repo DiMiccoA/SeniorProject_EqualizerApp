bf2a361db5616ad0ee4c66353a8620eabaf480d5
patrickoliver <oliverpa@onid.oregonstate.edu>
2014-02-19 11:13:59 -0800

    [WIP] Equalizer Block Comment - not working
    Instantiating the Equalizer doesn't seem to work. We're planning on
    fixing that by attaching to another audio output besides the global
    audio output.

--- Eclipse/src/com/fourears/equalizerapp/MainActivity.java
+++ Eclipse/src/com/fourears/equalizerapp/MainActivity.java
@@ -6,16 +6,24 @@ import android.app.Activity;
 import android.view.Menu;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.view.*;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
 	public SeekBar sb1,sb2,sb3,sb4,sb5;
 	private Equalizer equaliz;
+	private LinearLayout mLinearLayout;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		mLinearLayout = new LinearLayout(this);
+        mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        setContentView(mLinearLayout);
+		//setContentView(R.layout.activity_main);
 		setupEqualizerAndUI();
 	}
 
@@ -27,15 +35,75 @@ public class MainActivity extends Activity {
 	}
 	
 	private void setupEqualizerAndUI(){
+		//Create and attach a new Equalizer to the global audio stream
 		equaliz = new Equalizer(0,0);
-		equaliz.setEnabled(true);
+		/*equaliz.setEnabled(true);
+		
 		
 		TextView eqTextView = new TextView(this);
         eqTextView.setText("Equalizer:");
+        mLinearLayout.addView(eqTextView);
+/*
+        short bands = equaliz.getNumberOfBands();      
+        final short minEQLevel = equaliz.getBandLevelRange()[0];
+        final short maxEQLevel = equaliz.getBandLevelRange()[1];
+        
+        for(short i=0; i<bands; i++){
+        	final short band = i;
+        	
+        	//Setup view for frequency range
+        	TextView freqTextView = new TextView(this);
+            freqTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            freqTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+            freqTextView.setText((equaliz.getCenterFreq(band) / 1000) + " Hz");
+            mLinearLayout.addView(freqTextView);
+            
+            LinearLayout row = new LinearLayout(this);
+            row.setOrientation(LinearLayout.HORIZONTAL);
+            
+            //Setup view for min Db
+            TextView minDbTextView = new TextView(this);
+            minDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            minDbTextView.setText((minEQLevel / 100) + " dB");
+            
+            //Set up view for max Db
+            TextView maxDbTextView = new TextView(this);
+            maxDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            maxDbTextView.setText((maxEQLevel / 100) + " dB");
+            
+            //Setup seekbar
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.weight = 1;
+            SeekBar bar = new SeekBar(this);
+            bar.setLayoutParams(layoutParams);
+            bar.setMax(maxEQLevel - minEQLevel);
+            bar.setProgress(equaliz.getBandLevel(band));
+            
+            //Add listeners for each seekbar
+            bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                        boolean fromUser) {
+                    equaliz.setBandLevel(band, (short) (progress + minEQLevel));
+                }
 
-        short bands = equaliz.getNumberOfBands();
-        
-        
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+            
+            row.addView(minDbTextView);
+            row.addView(bar);
+            row.addView(maxDbTextView);
+
+            mLinearLayout.addView(row);
+        }*/
         
 	}
 
